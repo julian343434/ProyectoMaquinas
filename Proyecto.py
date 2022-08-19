00226565fe8d683910acaee9001b85d9ed17c651
deivid-julian <julher2000@hotmail.es>
2022-08-19 07:58:21 -0500

    poniendo el gif ANIMACION-ABRIENDOSE

--- Proyecto.py
+++ Proyecto.py
@@ -1,3 +1,4 @@
+from re import ASCII
 import pygame , sys
 pygame.init()
 
@@ -15,17 +16,22 @@ size= (400,1500)
 #crear ventana 
 screen= pygame.display.set_mode(size)
 
-class Ascensor(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load("ANIMACION-ABRIENDOSE.gif").convert()    
-        self.image.set_colorkey(BLACK)
-        self.rect=self.image.get_rect()
-        self.sprites.append(pygame.image.load('ANIMACION-ABRIENDOSE.gif'))
+#cargando imagen
+ascensor= pygame.image.load("ANIMACION-ABRIENDOSE.gif")
 
-Ascensor = Ascensor()
-Ascensor.rect.x=200
-Ascensor.rect.y=750 
+
+
+#class Ascensor(pygame.sprite.Sprite):
+    #def __init__(self):
+     #   super().__init__()
+     #   self.image = pygame.image.load("ANIMACION-ABRIENDOSE.gif").convert()    
+     #   self.image.set_colorkey(BLACK)
+     #   self.rect=self.image.get_rect()
+     #   self.sprites.append(pygame.image.load('ANIMACION-ABRIENDOSE.gif'))
+
+#Ascensor = Ascensor()
+#Ascensor.rect.x=200
+#Ascensor.rect.y=750 
 
 
 while True:
@@ -37,7 +43,7 @@ while True:
     screen.fill(WHITE)
 
     ##---- ZONA DE DIBUJO
-
+    screen.blit(ANIMACION-ABRIENDOSE.gif,(200,200))
 
     #for i in range(100,700,100)
         #pygame.draw.rect(screen,BLACK,(x,230,50,50))
