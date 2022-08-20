c0202b6dd0dd127f1343421f709af32c5fd9c9fd
julian53 <julher2000@hotmail.es>
2022-08-20 00:22:54 -0500

    haciendo la primera animacion

--- Proyecto.py
+++ Proyecto.py
@@ -1,73 +1,43 @@
-from re import ASCII
-import pygame , sys
-pygame.init()
+from pygame import *
+import sys ,time
+from time import sleep
+WHITE=(255,255,255)
 
-#definen los colores
+def cargar_animacion(prefijo,sufijo,n):
+    images=[]
+    for i in range(1,n+1):
+        name=prefijo+str(i)+sufijo
+        images.append(image.load(name))
+    return images
 
-BLACK   =(0,0,0)
-WHITE   =(255,255,255)
-GREEN   =(0,255,0)
-RED     =(255,0,0)
-BLUE    =(0,0,255)
+def mostrar_animacion(screen,images,freq,x,y,m):
+    frame=int(time.time()*freq) %len(images)
+    screen.blit(images[frame],(x,y))   
 
+init()
 
-size= (400,1500)
+ancho=1400
+largo=800
 
-#crear ventana 
-screen= pygame.display.set_mode(size)
-
-#cargando imagen
-ascensor= pygame.image.load("ANIMACION-ABRIENDOSE.gif")
-<<<<<<< HEAD
-
-
-
-#class Ascensor(pygame.sprite.Sprite):
-    #def __init__(self):
-     #   super().__init__()
-     #   self.image = pygame.image.load("ANIMACION-ABRIENDOSE.gif").convert()    
-     #   self.image.set_colorkey(BLACK)
-     #   self.rect=self.image.get_rect()
-     #   self.sprites.append(pygame.image.load('ANIMACION-ABRIENDOSE.gif'))
-
-=======
+screen=display.set_mode((ancho,largo))
+fondo=image.load('./AscensorImagenes/Fondo.png')
+AscensorIma= image.load('./AscensorImagenes/Cerrado1.png')
+Abriendo=cargar_animacion("./AscensorImagenes/Cerrado",".png",3)
 
 
-
-#class Ascensor(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load("ANIMACION-ABRIENDOSE.gif").convert()    
-        self.image.set_colorkey(BLACK)
-        self.rect=self.image.get_rect()
-        self.sprites.append(pygame.image.load('ANIMACION-ABRIENDOSE.gif'))
-
->>>>>>> origin
-#Ascensor = Ascensor()
-#Ascensor.rect.x=200
-#Ascensor.rect.y=750 
-
-
+#images=[]
+#for i in range(1,4):
+#    name= "./AscensorImagenes/Cerrado"+str(i)+".png"
+#    images.append(image.load(name))
+cantidad =0
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-    
-    #colorea la pantalla
-    screen.fill(WHITE)
+    screen.fill((255,255,255))
+    for e in event.get():
+        if e.type == QUIT: sys.exit()
+    #frame=int(time.time()*2)% len(images)   
+    screen.blit(fondo,(0,0))
+    screen.blit(AscensorIma,((ancho/2)-(150/2),largo-200))
+    mostrar_animacion(screen,Abriendo,2,100,100,1)   
+    #screen.blit(images[frame],(0,0))
 
-    ##---- ZONA DE DIBUJO
-    screen.blit(ANIMACION-ABRIENDOSE.gif,(200,200))
-
-    #for i in range(100,700,100)
-        #pygame.draw.rect(screen,BLACK,(x,230,50,50))
-
-    #pygame.draw.line(screen,GREEN,[0,100],[100,100],5)
-    #pygame.draw.rect(screen,BLACK,(100,100,80,80))
-    #pygame.draw.circle(screen,BLACK,(200,200),30)
-    
-    
-    ##---- ZONA DE DIBUJO
-
-    #actualiza la pantalla
-    pygame.display.flip()
+    display.flip()
